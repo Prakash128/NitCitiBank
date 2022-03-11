dd94dc0de644b648ca9db2e4f5086ad72b0cba87
Prakash128 <prakashbehera3344@gmail.com>
2022-03-11 19:01:59 +0530

    chnages

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Utility {
 	public Date GetSysDate() {
-		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 		return new Date();
 	}
 
